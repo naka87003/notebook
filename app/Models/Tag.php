34d0bcc59ee0b867972b29e3d559412d7f397940
naka87003 <naka87003@outlook.jp>
2024-08-28 07:56:03 +0000

    first commit

---
+++ app/Models/Tag.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [];
+
+    /**
+     * Tagに所属するNoteをまとめて取得
+     */
+    public function notes(): HasMany
+    {
+        return $this->hasMany(Note::class);
+    }
+}
