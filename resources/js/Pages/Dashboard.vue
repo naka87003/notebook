36e8e188742026b179df4a932553866d4be6114e
naka87003 <naka87003@outlook.jp>
2024-08-29 12:33:58 +0900

    tooltipにclose-on-content-clickを付与

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -34,7 +34,7 @@ const dialog = ref({
   archiveConfirm: false,
   retrieveConfirm: false,
   deleteConfirm: false,
-  searchText:false,
+  searchText: false,
   sortMenu: false,
   filterMenu: false,
   enlargedImage: false,
@@ -272,20 +272,20 @@ provide('updatePosts', updatePosts);
       </v-text-field>
       <v-btn class="hidden-sm-and-up" @click="dialog.searchText = true">
         <v-icon size="x-large" icon="mdi-magnify" :class="{ 'text-red': searchEntered }" />
-        <v-tooltip activator="parent" location="bottom" text="Search" />
+        <v-tooltip activator="parent" location="bottom" text="Search" close-on-content-click />
       </v-btn>
       <v-spacer></v-spacer>
       <v-btn @click="dialog.create = true">
         <v-icon size="x-large" icon="mdi-plus" />
-        <v-tooltip activator="parent" location="bottom" text="New" />
+        <v-tooltip activator="parent" location="bottom" text="New" close-on-content-click />
       </v-btn>
       <v-btn :class="{ 'text-red': sortChanged }" @click="dialog.sortMenu = true">
         <v-icon size="x-large" :icon="sortIcon" />
-        <v-tooltip activator="parent" location="bottom" text="Sort" />
+        <v-tooltip activator="parent" location="bottom" text="Sort" close-on-content-click />
       </v-btn>
       <v-btn :class="{ 'text-red': filterChanged }" @click="dialog.filterMenu = true">
         <v-icon size="x-large" icon="mdi-filter-menu-outline" />
-        <v-tooltip activator="parent" location="bottom" text="Filter" />
+        <v-tooltip activator="parent" location="bottom" text="Filter" close-on-content-click />
       </v-btn>
     </template>
     <v-container>
@@ -299,7 +299,8 @@ provide('updatePosts', updatePosts);
           Let's click the plus button above to create your first note!
         </v-alert>
       </template>
-      <v-alert v-else-if="isInProgress === false && notes.size === 0" variant="text" class="text-center" text="No data available" />
+      <v-alert v-else-if="isInProgress === false && notes.size === 0" variant="text" class="text-center"
+        text="No data available" />
       <v-infinite-scroll v-else :onLoad="load" class="w-100 overflow-hidden" empty-text="">
         <v-row>
           <template v-for="note in notes.values()" :key="note.id">
@@ -309,19 +310,19 @@ provide('updatePosts', updatePosts);
                 <template #actions>
                   <v-btn size="small" @click="showEditDialog(note)">
                     <v-icon size="large" icon="mdi-pencil-outline" />
-                    <v-tooltip activator="parent" location="bottom" text="Edit" />
+                    <v-tooltip activator="parent" location="bottom" text="Edit" close-on-content-click />
                   </v-btn>
                   <v-btn v-if="note.status.name === 'archived'" size="small" @click="showRetrieveConfirmDialog(note)">
                     <v-icon size="large" icon="mdi-keyboard-return" />
-                    <v-tooltip activator="parent" location="bottom" text="Retrieve" />
+                    <v-tooltip activator="parent" location="bottom" text="Retrieve" close-on-content-click />
                   </v-btn>
                   <v-btn v-else size="small" @click="showArchiveConfirmDialog(note)">
                     <v-icon size="large" icon="mdi-archive-plus-outline" />
-                    <v-tooltip activator="parent" location="bottom" text="Archive" />
+                    <v-tooltip activator="parent" location="bottom" text="Archive" close-on-content-click />
                   </v-btn>
                   <v-btn size="small" @click="showDeleteConfirmDialog(note)">
                     <v-icon size="large" icon="mdi-delete-outline" />
-                    <v-tooltip activator="parent" location="bottom" text="Delete" />
+                    <v-tooltip activator="parent" location="bottom" text="Delete" close-on-content-click />
                   </v-btn>
                 </template>
               </NoteItem>
