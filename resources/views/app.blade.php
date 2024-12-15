ba3865c8db6a2892d68969c713008b0b0b64f48c
naka87003 <naka87003@outlook.jp>
2024-12-15 09:52:09 +0900

    OGPの設定を追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- OGP -->
+    <meta property="og:url" content="{{ env('app.url') }}">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Feel free to write notes and share them with others.">
+    <meta property="og:image" content="{{ asset('/notebook.jpeg') }}">
 
-        <link rel="icon" href="{{ asset('/favicon.ico') }}">
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <link rel="icon" href="{{ asset('/favicon.ico') }}">
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
