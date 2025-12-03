956a547050364e89eff40d0427ddb597be7b5add
naka87003 <naka87003@outlook.jp>
2025-12-03 09:16:11 +0900

    VCalendarはlabsでは無くなったので削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,12 +12,10 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import { VCalendar } from 'vuetify/labs/VCalendar';
 
 const vuetify = createVuetify({
   components: {
     ...components,
-    VCalendar,
   },
   directives,
   icons: {
